e6a3d8bc22a9c743ad385e8257a78e7a07231896
Jacob Kepford <rtbredge@gmail.com>
2022-08-22 19:48:23 -0500

    added admin-upload page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -13,8 +14,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="title">
+          Upload <Link href="/admin/upload"> from this page!</Link>
         </h1>
 
         <p className={styles.description}>
