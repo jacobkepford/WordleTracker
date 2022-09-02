dd8e2b81ea5bfc32a465fbcccfd0a476cac6194a
Jacob Kepford <rtbredge@gmail.com>
2022-09-01 19:38:43 -0500

    prisma setup and working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import prisma from "../lib/prisma";
+
+// interface Score {
+//   score_id: number;
+// }
+// interface HomeProps {
+//   score: Score[];
+// }
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Wordle Tracker</title>
+        <title>Wordle Tracker </title>
         <meta name="description" content="Track your Wordle group scores" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -35,5 +43,16 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const score = await prisma.score.findMany({
+//     select: {
+//       score_id: true,
+//     },
+//   });
+//   return {
+//     props: { score },
+//   };
+// };
 
 export default Home;
