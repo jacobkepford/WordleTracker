63ed73272be1b9076dfcc3588041dcdd519bc2ee
Jacob Kepford <rtbredge@gmail.com>
2022-09-12 20:14:39 -0500

    User score data return logic setup but not working

--- api/GetUserScores.ts
+++ api/GetUserScores.ts
@@ -1,7 +1,55 @@
+type ScoreData = {
+    score_id: number,
+    score_value: number,
+    score_user: number,
+    score_date: Date
+}
+
+export type UserScore = {
+    "One": number,
+    "Two": number,
+    "Three": number,
+    "Four":  number,
+    "Five":  number,
+    "Six": number
+}
+
 const GetUserScores = async (userID: number) => {
     const response = await fetch(`http://localhost:3000/api/score?id=${userID}`);
     const data = await response.json();
-    return data;
+
+    const scoreData: UserScore = {
+        "One": 0,
+        "Two": 0,
+        "Three": 0,
+        "Four": 0,
+        "Five": 0,
+        "Six": 0
+    };
+
+    data.score.forEach((score: ScoreData) => {
+        switch (score.score_value) {
+            case 1:
+                scoreData.One = scoreData.One + 1;
+                break;
+            case 2:
+                scoreData.Two = scoreData.Two + 1;
+                break;
+            case 3:
+                scoreData.Three = scoreData.Three + 1;
+                break;
+            case 4:
+                scoreData.Four = scoreData.Four + 1;
+                break;
+            case 5:
+                scoreData.Five = scoreData.Five + 1;
+                break;  
+            case 6:
+                scoreData.Six = scoreData.Six + 1;
+                break; 
+        }
+    });
+    return scoreData;
 }
 
 export default GetUserScores;
--- pages/userscore/[id].tsx
+++ pages/userscore/[id].tsx
@@ -1,25 +1,27 @@
 import { GetServerSideProps, NextPage } from "next";
-import GetUserScores from "../../api/GetUserScores";
-
-type Score = {
-  score_id: number;
-  score_value: number;
-  score_user: number;
-  score_date: Date;
-};
+import GetUserScores, { UserScore } from "../../api/GetUserScores";
 
 type UserScoreProps = {
-  score: Score[];
+  scores: UserScore;
 };
 
-const UserScore = ({ score }: UserScoreProps) => {
-  return <div>Hello {score[0].score_id}</div>;
+const UserScore = ({ scores }: UserScoreProps) => {
+  return (
+    <div>
+      <h2>{`One: ${scores.One}`}</h2>
+      <h2>{`Two: ${scores.Two}`}</h2>
+      <h2>{`Three: ${scores.Three}`}</h2>
+      <h2>{`Four: ${scores.Four}`}</h2>
+      <h2>{`Five: ${scores.Five}`}</h2>
+      <h2>{`Six: ${scores.Six}`}</h2>
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const score = await GetUserScores(1);
+  const scores = await GetUserScores(1);
   return {
-    props: score,
+    props: scores,
   };
 };
 
